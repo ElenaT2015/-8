3240bc3e3a07fc581e7d6cd77a4cbd491dc6d8bf
Tsygankovae.a <Tsygankovae.a@outlook.com>
2021-12-13 20:21:11 +0300

    Добавьте файлы проекта.

---
+++ Задание 8.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mypath = "myfile.txt";
+            Random random = new Random();
+            const int c10 = 10;
+            int sum = 0;
+            StreamWriter saveNumber = new StreamWriter(mypath, false);
+            int[] numberRand = new int[c10];
+            int[] numbertoFile = new int[c10];
+            if (!File.Exists(mypath))
+            {
+                File.Create(mypath);
+            }
+            for (int i = 0; i < c10; i++)
+            {
+                numberRand[i] = random.Next(1, 100);
+                saveNumber.WriteLine(numberRand[i]);
+                saveNumber.Flush();
+            }
+            saveNumber.Close();
+            StreamReader readNumber = new StreamReader(mypath);
+            for (int i = 0; i < c10; i++)
+            {
+                numbertoFile[i] = Convert.ToInt32(readNumber.ReadLine());
+            }
+            readNumber.Close();
+            for (int i = 0; i < c10; i++)
+            {
+                sum += numbertoFile[i];
+            }
+            Console.WriteLine(" Сумма в файле {0} = {1}", mypath, sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathD = "C:/";
+            string[] directory = Directory.GetDirectories(pathD);
+            string[] files = Directory.GetFiles(pathD);
+            Directory.SetCurrentDirectory(pathD);
+            if (Directory.Exists(pathD))
+            {
+                foreach (string dir in directory)
+                {
+                    Console.WriteLine(dir);
+
+                }
+                foreach (string fil in files)
+                {
+                    Console.WriteLine(fil);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Дирректория {0}: не существует", pathD);
+            }
+            Console.ReadKey();
+        }
+    }
+}
